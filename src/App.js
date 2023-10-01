1a5e87dfcc7c3d174229f391cdc778af906bad7d
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2023-10-01 21:49:32 +0300

    init: start new project: define project structure and implement routing

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import {
+  createBrowserRouter, 
+  createRoutesFromElements,
+  Route, 
+  RouterProvider
+} from 'react-router-dom'
+import NotFound from './pages/NotFound';
+import Main from './pages/Main';
+import FormBuilder from './pages/FormBuilder';
+import RootLayout from './layouts/RootLayout';
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Main />} />
+      <Route path="form" element={<FormBuilder />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
+function App() {
+  return (
+    <RouterProvider router={router} />
+  );
+}
+
+export default App;
