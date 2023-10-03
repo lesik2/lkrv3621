8d61fff1cb28681c81f90e05a8190a28a25de073
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2023-10-03 10:54:35 +0300

    feat: add navigation to form page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import MyButton from '../components/ui/MyButton'
+import {useNavigate} from 'react-router-dom'
 
 export default function Main() {
+  const navigate = useNavigate();
+  const handleClicked =() => {
+    setTimeout(()=>{
+      navigate("form");
+    },400)
+  }
   return (
     <div style={{ height: '100vh',display: 'flex', justifyContent: 'center','alignItems':'center', backgroundColor: '#fff2e6'}}>
-      <MyButton text='Form' onClick={()=>console.log('hi')}/>
+        <MyButton text='Form' onClick={handleClicked}/>
     </div>
 
   )
